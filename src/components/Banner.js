0b0fe17da7b3283e9552f3aa7c7f83a4b9931244
Artash <artash92@mail.ru>
2023-04-10 18:06:01 +0400

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,24 @@
+import React from "react";
+//import Images from "../assets/images/banner-bg.jpg";
+
+function Banner(props) {
+  return (
+    <div>
+    {/* <!-- ======= Hero Section ======= --> */}
+<div id="hero" className="hero route bg-image" >
+ <div className="overlay-itro"></div>
+ <div className="hero-content display-table">
+   <div className="table-cell">
+     <div className="container">
+       <h1 className="hero-title mb-4">Join Our Adventure</h1>
+     </div>
+   </div>
+ </div>
+</div>
+{/* <!-- End Hero Section --> */}
+
+ </div>
+  );
+}
+
+export default Banner;
